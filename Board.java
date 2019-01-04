332e88dbf1698dffcaa371dab607ad8cb59ca1d7
unknown <azheng00@stuy.edu>
2019-01-03 22:47:29 -0500

    made the Start Game Look, still need to make some of the things be user controlled

--- Board.java
+++ Board.java
@@ -1,1 +1,38 @@
-kjlsajfp
+public class Board{
+  public static void StartGame(){
+    int c=10;
+    while (c!=0){
+      int count=10;
+      while (count!=0){
+        System.out.print("+---");
+        count--;
+      }
+      System.out.println("+");
+      count=10;
+      while (count!=0){
+        System.out.print("|   ");
+        count--;
+      }
+      if (c==7){
+        System.out.println("|           SCORE:0");
+      }
+      else if(c==5){
+        System.out.println("|           RESTART");
+      }
+      else{
+        System.out.println("|");
+      }
+      c--;
+    }
+    c=10;
+    while (c!=0){
+      System.out.print("+---");
+      c--;
+    }
+    System.out.println("+"+"\n"+"\n"+"\n");
+    System.out.println("Start Game?");
+  }
+  public static void main(String[] args) {
+    StartGame();
+  }
+}
