faddbad5b315934c851833f99ca8ccce9b0f2643
Amanda Chen <achen01@stuy.edu>
2019-01-04 11:58:12 -0500

    wrote contructor for 1x1 block

--- Block.java
+++ Block.java
@@ -3,4 +3,45 @@ public class Block {
   private Square[][] block;
   private int numBlocks;
   private int pos;
+
+  public Block(String shape, int pos, String color){
+    if (shape.equals("1block")){
+      block = new Square[1][1];
+      block[1][1] = new Square(color, 0, 0);
+    }
+    if (shape.equals("2block")){
+      if (pos == 1){
+
+      } else {
+
+      }
+    }
+    if (shape.equals("3block")){
+      if (pos == 1){
+
+      } else {
+
+      }
+    }
+    if (shape.equals("4block")){
+      if (pos == 1){
+
+      } else {
+
+      }
+    }
+    if (shape.equals("5block")){
+      if (pos == 1){
+
+      } else {
+
+      }
+    }
+    if (shape.equals("4sqaure"){
+
+    }
+    if (shape.equals("9sqaure")){
+
+    }
+  }
 }
