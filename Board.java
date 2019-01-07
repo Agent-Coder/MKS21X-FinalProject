29e2f4183b97e359be9297fe5147ca9298a3ed3c
Amanda Chen <achen01@stuy.edu>
2019-01-06 22:18:08 -0500

    added lantera functionality

--- Board.java
+++ Board.java
@@ -1,3 +1,18 @@
+//API : http://mabe02.github.io/lanterna/apidocs/2.1/
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+
 public class Board{
   private int[][] board;
   private int spotCount;
@@ -5,8 +20,8 @@ public class Board{
   private int score;
 
 
-  public static String StartGame(){
-    String s="";
+  public static String StartGame(int x, int y){
+    /*String s="";
     int c=10;
     while (c!=0){
           s=s+"+---+---+---+---+---+---+---+---+---+---+\n";
@@ -23,10 +38,41 @@ public class Board{
     }
     s=s+"+---+---+---+---+---+---+---+---+---+---+\n"+"\n"+"\n";
     s=s+"PRESS ENTER TO START GAME";
-    return s;
+    return s;*/
+    return "";
+
+
   }
   public static void main(String[] args) {
-    System.out.println(StartGame());
+    int x = 0;
+		int y = 0;
+
+    Terminal terminal = TerminalFacade.createTextTerminal();
+		terminal.enterPrivateMode();
+
+		TerminalSize size = terminal.getTerminalSize();
+		terminal.setCursorVisible(false);
+
+    boolean running = true;
+
+    StartGame(x,y);
+
+    while(running){
+      terminal.moveCursor(x,y);
+      terminal.applyBackgroundColor(Terminal.Color.WHITE);
+			terminal.applyForegroundColor(Terminal.Color.BLACK);
+      terminal.putCharacter('\u00a4');
+
+      Key key = terminal.readInput();
+
+			if (key != null){
+				if (key.getKind() == Key.Kind.Escape) {
+					terminal.exitPrivateMode();
+					running = false;
+				}
+      }
+    }
+
   }
   //looping thorugh and adding the layout of the obard for background
 }
