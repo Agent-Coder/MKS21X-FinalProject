a56d1e64795bafd1277a20743896c05eb8312f10
Amanda Chen <achen01@stuy.edu>
2019-01-07 23:27:55 -0500

    created board constructor

--- Board.java
+++ Board.java
@@ -6,6 +6,11 @@ public class Board {
 
   public Board(){
     Square[][] board = new Square[10][10];
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[0].length; j++){
+        board[i][j] = new Square("black", 0, 0);
+      }
+    }
   }
 
   public String toString(){
