a5518896197a19c36f6cc261d237a8b9c1f04249
Amanda Zheng <azheng00@stuy.edu>
2019-01-09 10:52:06 -0500

    edited place blocks so an array used for printing easier is made

--- Board.java
+++ Board.java
@@ -3,15 +3,16 @@ public class Board {
   private int blockCount;
   private int score;
   private Square[][] board;
+  private boolean[][] layout;
 
   public Board(){
     board = new Square[10][10];
-    /*for (int i = 0; i < 10; i++){
-      for (int j = 0; j < 10; j++){
-        board[i][j] = new Square("black", i, j);
-        //System.out.println(board[i][j]);
+    layout= new boolean[21][10];
+    for (int a=0;a<layout.length;a++){
+      for (int b=0;b<layout[0].length;b++){
+        layout[a][b]=false;
       }
-    }*/
+    }
   }
 
   public static String PrintSelection(boolean[][] selection){
@@ -46,131 +47,132 @@ public class Board {
 
       return entire;
     }
-public static Block generateBlock(){
-    int roll=(int)(Math.random()*100)%3;
-    int length;
-    Block random;
-    if(roll==0){
-      roll=(int)(Math.random()*100)%3+1;
-      random=new FullBlock(roll);
-    }
-    else if(roll==1){
-      roll=(int)(Math.random()*100)%2;
-      length=(int)(Math.random()*100)%4+2;
-      random=new LongBlock(length,roll);
-    }
-    else{
-      roll=(int)(Math.random()*100)%4;
-      length=(int)(Math.random()*100)%2+2;
-      random=new LBlock(length,roll);
-    }
-    return random;
-  }
 
-  public Square getSquare(int x, int y){
-    return board[x][y];
-  }
+    public static Block generateBlock(){
+      int roll=(int)(Math.random()*100)%3;
+      int length;
+      Block random;
+      if(roll==0){
+        roll=(int)(Math.random()*100)%3+1;
+        random=new FullBlock(roll);
+      }
+      else if(roll==1){
+        roll=(int)(Math.random()*100)%2;
+        length=(int)(Math.random()*100)%4+2;
+        random=new LongBlock(length,roll);
+      }
+      else{
+        roll=(int)(Math.random()*100)%4;
+        length=(int)(Math.random()*100)%2+2;
+        random=new LBlock(length,roll);
+      }
+      return random;
+    }
 
-  public static boolean[][] blockSelection(Block a,Block b, Block c){
-    boolean[][] selection=new boolean[11][a.getCol()+b.getCol()+c.getCol()+4];
-    int x=0,y=0;
-    for(;x<11;x++){
-      for(;y<selection[0].length;y++){
-          selection[x][y]=false;
-      }
+    public Square getSquare(int x, int y){
+      return board[x][y];
     }
-    x=0;
-    y=0;
 
-    for(;x<a.getRow();x++){
-      y=0;
-      for(;y<a.getCol();y++){
-        selection [x][y]=a.getmap(x,y);
-      }
-    }
-    x=0;
-    y+=2;
-    int z=y;
-    for(;x<b.getRow();x++){
-      z=y;
-      for(;z<(b.getCol()+a.getCol()+2);z++){
-        selection [x][z]=b.getmap(x,z-y);
-      }
-    }
-    x=0;
-    z+=2;
-    for(;x<c.getRow();x++){
-      for(int w=z;w<selection[0].length;w++){
-        selection [x][w]=c.getmap(x,w-z);
-      }
-    }
-    return selection;
-  }
-
-  public boolean placeBlock(Block b, int x, int y){
-    if (placeable(b, x, y)){
-      for (int i = 0; i < b.getLength(); i++){
-        for (int j = 0; j < b.getWidth(); j++){
-          board[x][y] = b.getBlock()[i][j];
-          y++;
+    public static boolean[][] blockSelection(Block a,Block b, Block c){
+      boolean[][] selection=new boolean[11][a.getCol()+b.getCol()+c.getCol()+4];
+      int x=0,y=0;
+      for(;x<11;x++){
+        for(;y<selection[0].length;y++){
+            selection[x][y]=false;
         }
-        x++;
+      }
+      x=0;
+      y=0;
+
+      for(;x<a.getRow();x++){
+        y=0;
+        for(;y<a.getCol();y++){
+          selection [x][y]=a.getmap(x,y);
+        }
+      }
+      x=0;
+      y+=2;
+      int z=y;
+      for(;x<b.getRow();x++){
+        z=y;
+        for(;z<(b.getCol()+a.getCol()+2);z++){
+          selection [x][z]=b.getmap(x,z-y);
+        }
+      }
+      x=0;
+      z+=2;
+      for(;x<c.getRow();x++){
+        for(int w=z;w<selection[0].length;w++){
+          selection [x][w]=c.getmap(x,w-z);
+        }
+      }
+      return selection;
+    }
+
+    public boolean placeBlock(Block b, int x, int y){
+      int tempx=x;
+      int tempy=y;
+      if (placeable(b, x, y)){
+        for (int i = 0; i < b.getLength(); i++){
+          for (int j = 0; j < b.getWidth(); j++){
+            board[tempx][tempy] = b.getBlock()[i][j];
+            tempy++;
+          }
+          tempx++;
+        }
+        layout=new boolean[21][10];
+        for (int r=0;r<b.getRow();r++){
+          for (int s=0;s<b.getCol();s++){
+            layout[x][y]=b.getmap(r,s);
+          }
+        }
+        return true;
+      }
+      return false;
+    }
+
+    private boolean placeable(Block bl, int x, int y){
+      int a = 0;
+      int b = 0;
+      //System.out.println(board.length);
+      //System.out.println(y+bl.getWidth());
+      for (int i = x; a < bl.getLength() && i < x+bl.getLength(); i++){
+        a = 0;
+        for (int j = y; b < bl.getWidth() && j < y+bl.getWidth(); j++){
+          b = 0;
+          //System.out.println("a: " + a + "\nb: " + b + "\ni: " + i + "\nj: " + j);
+          //System.out.println(board[0][0]);
+          //System.out.println(bl.getBlock()[a][b] != null);
+          if (board[i][j] != null && bl.getBlock()[a][b] != null){
+            return false;
+          }
+          b++;
+        }
+        a++;
       }
       return true;
     }
-    return false;
-  }
 
-  private boolean placeable(Block bl, int x, int y){
-    int a = 0;
-    int b = 0;
-    //System.out.println(board.length);
-    //System.out.println(y+bl.getWidth());
-    for (int i = x; a < bl.getLength() && i < x+bl.getLength(); i++){
-      a = 0;
-      for (int j = y; b < bl.getWidth() && j < y+bl.getWidth(); j++){
-        b = 0;
-        //System.out.println("a: " + a + "\nb: " + b + "\ni: " + i + "\nj: " + j);
-        //System.out.println(board[0][0]);
-        //System.out.println(bl.getBlock()[a][b] != null);
-        if (board[i][j] != null && bl.getBlock()[a][b] != null){
-          return false;
-        }
-        b++;
+    public String toString(){
+      String s="";//this is the string containing the entire board
+      int c=10;
+      while (c!=0){
+            s=s+"+---+---+---+---+---+---+---+---+---+---+\n";
+            if (c==7){
+              s=s+"|   |   |   |   |   |   |   |   |   |   |           SCORE:0\n";
+            }
+            else if(c==5){
+              s=s+"|   |   |   |   |   |   |   |   |   |   |           (Press Tab for Menu)\n";
+            }
+            else{
+            s=s+"|   |   |   |   |   |   |   |   |   |   |\n";
+          }
+          c--;
       }
-      a++;
+      s=s+"+---+---+---+---+---+---+---+---+---+---+\n"+"\n"+"\n";
+      return s;
     }
-    return true;
-  }
 
-  public String toString(){
-    String s="";//this is the string containing the entire board
-    int c=10;
-    while (c!=0){
-          s=s+"+---+---+---+---+---+---+---+---+---+---+\n";
-          if (c==7){
-            s=s+"|   |   |   |   |   |   |   |   |   |   |           SCORE:0\n";
-          }
-          else if(c==5){
-            s=s+"|   |   |   |   |   |   |   |   |   |   |           (Press Tab for Menu)\n";
-          }
-          else{
-          s=s+"|   |   |   |   |   |   |   |   |   |   |\n";
-        }
-        c--;
-    }
-    s=s+"+---+---+---+---+---+---+---+---+---+---+\n"+"\n"+"\n";
-    //s=s+"PRESS ENTER TO START GAME";
-    return s;
-
-    /*t.applyForegroundColor(Terminal.Color.BLACK);//the color of the board will be black
-    t.moveCursor(0,0);
-		for(int i = 0; i < s.length();i++){
-			t.putCharacter(s.charAt(i));
-		}*/
-
-
-  }
   public static void main(String[] args) {
     Block a=generateBlock();
     Block b=generateBlock();
