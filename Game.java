a921b53c54b4ca43b581763c99a139a42ea0dfa1
Amanda Chen <achen01@stuy.edu>
2019-01-10 00:55:56 -0500

    fixing commits

--- Game.java
+++ Game.java
@@ -22,25 +22,53 @@ public class Game{
 		}
 	}
 
-  public static void drawStartingScreen(Terminal t){
+  public static void drawStartingScreen(Terminal t, TerminalSize s){
+    String text = "1010!";
+    String text2 = "Press ENTER to START GAME";
+    int r = s.getColumns()/2 - text.length()/2;
+    int c = 0;
 
+    t.applyForegroundColor(Terminal.Color.BLACK);
+    putString(r,c,t,text);
+    r = s.getColumns()/2 - text2.length()/2;
+    c = 10;
+    t.applySGR(Terminal.SGR.ENTER_BLINK);
+    putString(r,c,t,text2);
+    t.applySGR(Terminal.SGR.EXIT_BLINK);
   }
 
-  public static void startGame(Terminal t, String s){
+  public static void drawBoard(Terminal t, String s){
     t.applyForegroundColor(Terminal.Color.BLACK);//the color of the board will be black
-    t.moveCursor(0,0);
-		for(int i = 0; i < s.length();i++){
-			t.putCharacter(s.charAt(i));
-		}
+    putString(0,0,t,s);
+  }
 
+  public static void drawBlockSelection(Terminal t, String s){
+    t.applyForegroundColor(Terminal.Color.GREEN);
+    putString(0,30,t,s);
+  }
 
+  public static int runGame(Terminal t, Board B, int numB){
+    Block a;
+    Block b;
+    Block c;
+    boolean[][] f = new boolean[0][0];
+    int num = numB;
+    if (num == 0){
+      a = B.generateBlock();
+      b = B.generateBlock();
+      c = B.generateBlock();
+      f = B.blockSelection(a,b,c);
+      num = 3;
+    }
+    drawBoard(t, B.toString());
+    drawBlockSelection(t, B.PrintSelection(f));
+    return num;
   }
 
   public static void main(String[] args) {
     Board game = new Board();
 
-    int x = 0;//coordinates for the cursor
-		int y = 0;
+    int numBlocks = 0;
 
     Terminal terminal = TerminalFacade.createTextTerminal();
 		terminal.enterPrivateMode();
@@ -51,8 +79,6 @@ public class Game{
     boolean running = true;
     int mode = 0;
 
-    //drawBoard(terminal);//the board will be drawn once only
-
     while(running){
 
 			terminal.applyForegroundColor(Terminal.Color.BLACK);
@@ -60,37 +86,33 @@ public class Game{
 
       Key key = terminal.readInput();
 
-			if (key != null){
 
-        if (mode == 0) {
-          drawStartingScreen(terminal);
+      if (mode == 0) {
+        drawStartingScreen(terminal, size);
+        if (key != null){
           if (key.getKind() == Key.Kind.Enter) {
-            startGame(terminal, game.toString());
+            terminal.clearScreen();
   					mode = 1;
-  				}
-        }
-
-        if (mode == 1){
-
-          if (key.getKind() == Key.Kind.Tab) {
-  					mode = 2;
-  				}
-        }
-
-        if (mode == 2){
-          if (key.getKind() == Key.Kind.Escape) {
-  					terminal.exitPrivateMode();
-  					running = false;
-  				}
-        }
-
-
-          if (key.getKind() == Key.Kind.Enter){//the first enter will erase the text
-            for (int i = 0; i < 25; i++){
-              terminal.moveCursor(i,24);
-              terminal.putCharacter(' ');
-            }
           }
+				}
+      }
+
+
+      if (mode == 1){
+        numBlocks = runGame(terminal, game, numBlocks);
+        if (key != null){
+          if (key.getKind() == Key.Kind.Tab) {
+            terminal.clearScreen();
+            mode = 1;
+          }
+
+        }
+      }
+      if (key != null){
+        if (key.getKind() == Key.Kind.Escape) {
+          terminal.exitPrivateMode();
+          running = false;
+        }
       }
 
     }
