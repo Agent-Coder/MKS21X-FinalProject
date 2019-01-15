8f85bde355bdbf3d93fd9f532fecf056bd9a5652
Amanda Zheng <azheng00@stuy.edu>
2019-01-14 23:35:33 -0500

    score placement

--- Game.java
+++ Game.java
@@ -233,24 +233,24 @@ public class Game{
           if (selectedBlock == 1){
             terminal.applySGR(Terminal.SGR.ENTER_BLINK);
             putBlock(terminal,a.toString(), selectedBlock,a.getColor());
-            terminal.moveCursor(0,0);
+            terminal.moveCursor(5,20);
             terminal.putCharacter(' ');
-            putString(0, 0, terminal, ""+game.getScore());
+            putString(5, 20, terminal, ""+game.getScore());
             terminal.applySGR(Terminal.SGR.EXIT_BLINK);
           }
           if (selectedBlock == 2){
             terminal.applySGR(Terminal.SGR.ENTER_BLINK);
-            terminal.moveCursor(0,0);
+            terminal.moveCursor(5,20);
             terminal.putCharacter(' ');
-            putString(0, 0, terminal, ""+game.getScore());
+            putString(5, 20, terminal, ""+game.getScore());
             putBlock(terminal,b.toString(), selectedBlock,b.getColor());
             terminal.applySGR(Terminal.SGR.EXIT_BLINK);
           }
           if (selectedBlock == 3){
             terminal.applySGR(Terminal.SGR.ENTER_BLINK);
-            terminal.moveCursor(0,0);
+            terminal.moveCursor(5,20);
             terminal.putCharacter(' ');
-            putString(0, 0, terminal, ""+game.getScore());
+            putString(5, 20, terminal, ""+game.getScore());
             putBlock(terminal,c.toString(), selectedBlock,c.getColor());
             terminal.applySGR(Terminal.SGR.EXIT_BLINK);
           }
