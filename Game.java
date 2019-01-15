6acabac0c51ccc762eba31dabc1c1b452b4ce215
Amanda Zheng <azheng00@stuy.edu>
2019-01-14 23:49:23 -0500

    deleted extra stuff

--- Game.java
+++ Game.java
@@ -233,23 +233,20 @@ public class Game{
           if (selectedBlock == 1){
             terminal.applySGR(Terminal.SGR.ENTER_BLINK);
             putBlock(terminal,a.toString(), selectedBlock,a.getColor());
-            terminal.moveCursor(58,6);
-            terminal.putCharacter(' ');
+
             putString(58,6, terminal, ""+game.getScore());
             terminal.applySGR(Terminal.SGR.EXIT_BLINK);
           }
           if (selectedBlock == 2){
             terminal.applySGR(Terminal.SGR.ENTER_BLINK);
-            terminal.moveCursor(58,6);
-            terminal.putCharacter(' ');
+
             putString(58,6, terminal, ""+game.getScore());
             putBlock(terminal,b.toString(), selectedBlock,b.getColor());
             terminal.applySGR(Terminal.SGR.EXIT_BLINK);
           }
           if (selectedBlock == 3){
             terminal.applySGR(Terminal.SGR.ENTER_BLINK);
-            terminal.moveCursor(58,6);
-            terminal.putCharacter(' ');
+      
             putString(58,6, terminal, ""+game.getScore());
             putBlock(terminal,c.toString(), selectedBlock,c.getColor());
             terminal.applySGR(Terminal.SGR.EXIT_BLINK);
